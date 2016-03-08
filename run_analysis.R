f262794719aece26f31609118232188b024d8ae3
Marcin K <knychala.marcin@gmail.com>
2016-03-08 01:11:37 +0100

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -14,7 +14,7 @@ library(dplyr)
 library(tidyr)
 
 
-##2.Extracts only the measurements on the mean and standard deviation for each measurement.
+##  Extracts only the measurements on the mean and standard deviation for each measurement.
 ##Filtering variables for those calculating mean and standard deviation
 meanFilter<-grep("mean()",features[,2], fixed=TRUE)
 stdFilter<-grep("std()",features[,2], fixed=TRUE)
@@ -25,9 +25,8 @@ filteredData2<-trainData[,varFilter]
 colnames(filteredData2)<-features[varFilter,2]
 
 
-##1.Merges the training and the test sets to create one data set.
-
-##I have done it the other way, just to join two smaller data sets.
+##  Merges the training and the test sets to create one data set.
+##I filtered out variables in data sets first, just to join two smaller data sets.
 filteredData<-rbind(filteredData1,filteredData2)
 ##I am also merging the same way two other tables - for Labels and Subjects
 jointLabels<-rbind(trainLabels,testLabels)
@@ -35,8 +34,7 @@ colnames(trainSubject)<-colnames(testSubject)
 jointSubjects<-rbind(trainSubject,testSubject)
 
 
-##3.Uses descriptive activity names to name the activities in the data set
-
+##  Uses descriptive activity names to name the activities in the data set
 ##For the table containing label data, I am changing the number into activity name
 label<-function(x) {as.character((filter(activities,V1==x)$V2))}
 for (i in seq_along(jointLabels$X5)) {
@@ -52,13 +50,12 @@ colnames(jointSubjects)<-"subject"
 filteredData<-cbind(filteredData,jointSubjects)
 
 
-##4.Appropriately labels the data set with descriptive variable names.
+##  Appropriately labels the data set with descriptive variable names.
 colnames(filteredData)<-gsub("[- .()]","",colnames(filteredData))
 colnames(filteredData)<-tolower(colnames(filteredData))
 
 
-##5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
+##  From the data set above, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 ##Melting data
 tempData<-gather(filteredData, subject, activty, tbodyaccmeanx:fbodybodygyrojerkmagstd)
 ##Changing column names
